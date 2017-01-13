5bce3d40161c7c7d06f36172f226ea5c5f0febbe
suga-ki <suga-ki@legendapl.com>
2017-01-13 14:42:45 +0900

    test

--- sample/java/src/main/Sample.java
+++ sample/java/src/main/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
   public static void main(String[] args) {
     System.out.println("This is version 0.1.1");
     System.out.println("by hata-sa");
+  	System.out.println("and suga-ki");
   }
 }
